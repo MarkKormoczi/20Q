5512ac8fce1fc326e5b892cd523ea324505a9137
Mark Kormoczi <mark.kormoczi@me.com>
2017-04-27 10:23:51 +0200

    Initial Commit

---
+++ 20Q/main.cpp
@@ -0,0 +1,15 @@
+//
+//  main.cpp
+//  20Q
+//
+//  Created by Mark Kormoczi on 2017. 04. 27..
+//  Copyright © 2017. Mark Kormoczi. All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
